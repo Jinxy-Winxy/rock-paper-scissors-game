c2194c6d7d109a0a95ad951009e4070a9a663e59
Jinxy-Winxy <206611320+Jinxy-Winxy@users.noreply.github.com>
2025-06-29 03:25:58 +0100

    added step 4 and 5

--- script.js
+++ script.js
@@ -33,3 +33,81 @@ function getHumanChoice(){
             return("scissors");
         }
 }
+// step 4
+
+let humanScore = 0;
+let computerScore = 0;
+
+// step 5 
+// write function takes computer and players score
+// plays a single round
+// increases score and logs a winner announcement
+
+function playRound(humanChoice, computerChoice){
+
+    if (humanChoice == "rock" && computerChoice == "rock"){
+        console.log("Tie! Rock vs Rock");
+    }
+        else if (humanChoice == "paper" && computerChoice == "paper"){
+        console.log("Tie! Paper vs Paper");
+        }
+        else if (humanChoice == "scissors" && computerChoice == "scissors"){
+        console.log("Tie! Scissors vs Scissors");
+        }
+        else if (humanChoice == "rock" && computerChoice == "scissors"){
+            console.log("Round Won! Rock beats Scissors");
+            humanScore++;
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock"){
+            console.log("Round Won! Paper beats Rock");
+            humanScore++;
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper"){
+            console.log("Round Won! Scissors beats Paper");
+            humanScore++;
+        }
+        else if (humanChoice == "rock" && computerChoice == "paper"){
+            console.log("Round Lose! Paper beats Rock")
+            computerScore++;
+        } 
+        else if (humanChoice == "paper" && computerChoice == "scissors"){
+            console.log("Round Lose! Scissors beats Paper");
+            computerScore++;
+        }
+        else {
+            console.log("Round Lose! Rock beats Scissors");
+            computerScore++;
+        }
+
+            console.log("Your score: ", humanScore);
+            console.log("Computer Score: ", computerScore);
+}
+
+    
+// step 6 
+// write new function called playGame that calls playRound
+// 5 rounds need to be played and to keep the score
+// declare who is the winner at the end.
+
+function playGame(){
+
+    playRound(getHumanChoice(),getComputerChoice());
+         playRound(getHumanChoice(),getComputerChoice());
+            playRound(getHumanChoice(),getComputerChoice());
+                 playRound(getHumanChoice(),getComputerChoice());
+                    playRound(getHumanChoice(),getComputerChoice());
+   
+
+
+    if (humanScore > computerScore){
+        console.log("You Win!");
+    }
+        else if (computerScore > humanScore){
+            console.log("Computer Wins!");
+        }
+        else {
+            console.log("Tie!")
+        }
+}
+
+playGame();
